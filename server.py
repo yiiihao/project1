7e71ccf2969259878e85c5e6c7fac5e148530f3d
yiiihao <91334036+yiiihao@users.noreply.github.com>
2022-04-13 16:15:56 -0400

    Update server.py

--- server.py
+++ server.py
@@ -180,7 +180,7 @@ def another():
 @app.route('/add', methods=['POST'])
 def add():
   name = request.form['name']
-  print name
+  print(name)
   cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
   g.conn.execute(text(cmd), name1 = name, name2 = name);
   return redirect('/')
