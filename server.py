286d27f7458095ec4132fe0bc1009859945c77ed
yiiihao <91334036+yiiihao@users.noreply.github.com>
2022-04-14 23:49:35 -0400

    Update server.py

--- server.py
+++ server.py
@@ -95,7 +95,7 @@ def index():
 
 #创建分页面1 奖牌
 @app.route('/medal_ranking')
- def medal_ranking():
+def medal_ranking():
 #   # DEBUG: this is debugging code to see what request looks like
 #   print(request.args) 
   
@@ -110,7 +110,7 @@ def index():
            
 #   context = dict(medal_data = medal_info)
   
-   return render_template("medal_ranking.html")
+    return render_template("medal_ranking.html")
 
 # 互动功能
 # 选择想要的奖牌信息 select information for medals
